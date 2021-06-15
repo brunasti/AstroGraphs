acf8c266a70e3328d28640e77a267450f75c4b2c
Paolo Brunasti <paolo.brunasti@booking.com>
2021-06-15 16:16:55 +0200

    Julia Set first view

--- src/it/brunasti/graph/fractals/JuliaSet.java
+++ src/it/brunasti/graph/fractals/JuliaSet.java
@@ -33,57 +33,68 @@ public class JuliaSet extends FractalBase {
  }
  */
 
+    // escape radius  # choose R > 0 such that R**2 - R >= sqrt(cx**2 + cy**2)
+    float r = 4f;
+    float cx = 1f;
+    float cy = 0f;
+
 
     @Override
     void setPrams() {
+        r = 4f;
+        cx = 0.3f;
+        cy = 0.1f;
+
         fromX = minX;
         toX = maxX;
         fromY = minY;
 
-        colorOption = 3;
+        colorOption = 0;
 
         gridFlag = true;
-        gridSize = 50;
+        gridSize = 20;
     }
 
+
+    /*
+zx = scaled x coordinate of pixel # (scale to be between -R and R)
+ # zx represents the real part of z.
+ zy = scaled y coordinate of pixel # (scale to be between -R and R)
+ # zy represents the imaginary part of z.
+
+ iteration = 0
+ max_iteration = 1000
+
+ while (zx * zx + zy * zy < R**2  AND  iteration < max_iteration)
+ {
+ xtemp = zx * zx - zy * zy
+ zy = 2 * zx * zy  + cy
+ zx = xtemp + cx
+
+ iteration = iteration + 1
+ }
+
+ if (iteration == max_iteration)
+ return black;
+ else
+ return iteration;
+     */
     @Override
-    void drawSet(Graphics g) {
-        var loopX = 0;
-        for (var px=0; px<FRAME_SIZE_X; px++) {
-            loopX ++;
-            var loopY = 0;
-            for (var py=0; py<FRAME_SIZE_Y; py++) {
-                var iteration = 0;
-                float x = 0;
-                float y = 0;
-                float x0 = ((px * (toX - fromX)) / FRAME_SIZE_X) + fromX;
-                float y0 = ((py * (toY - fromY)) / FRAME_SIZE_Y) + fromY;
+    int computeIteration(int px, int py) {
+        var iteration = 0;
 
-                while (((x*x + y*y) < 4) && (iteration < MAX_ITERATION)) {
-                    float xtemp = x*x - y*y + x0;
-                    y = 2*x*y + y0;
-                    x = xtemp;
-                    iteration = iteration + 1;
-                }
+        float x = 0;
+        float y = 0;
+        float zx = ((px * (toX - fromX)) / FRAME_SIZE_X) + fromX;
+        float zy = ((py * (toY - fromY)) / FRAME_SIZE_Y) + fromY;
 
-                g.setColor(pickColor(iteration));
-                g.drawLine(px, py, px, py);
-
-                loopY++;
-                if ((loopY >= gridSize) && (loopX >= gridSize)) {
-                    log("  x0:" + x0 + " y0:" + y0 + " iter:" + iteration);
-                    loopY = 0;
-                    if (gridFlag) {
-                        g.setColor(Color.blue);
-                        g.drawLine(px, py, px, py);
-                    }
-                }
-            }
-            if (loopX >= gridSize) {
-                log("  -------");
-                loopX = 0;
-            }
+        while (((zx*zx + zy*zy) < r*r) && (iteration < MAX_ITERATION)) {
+            float xtemp = zx*zx - zy*zy;
+            zy = 2*zx*zy + cy;
+            zx = xtemp + cx;
+            iteration = iteration + 1;
         }
+        return iteration;
     }
 
     public JuliaSet() throws HeadlessException {
--- src/it/brunasti/graph/fractals/Mandelbrot.java
+++ src/it/brunasti/graph/fractals/Mandelbrot.java
@@ -64,47 +64,65 @@ for each pixel (Px, Py) on the screen do
         }
     }
 
+//    @Override
+//    void drawSet(Graphics g) {
+////        Graphics2D g2d = (Graphics2D) g;
+////        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//
+//        var loopX = 0;
+//        for (var px=0; px<FRAME_SIZE_X; px++) {
+//            loopX ++;
+//            var loopY = 0;
+//            for (var py=0; py<FRAME_SIZE_Y; py++) {
+//                var iteration = 0;
+//                float x = 0;
+//                float y = 0;
+//                float x0 = ((px * (toX - fromX)) / FRAME_SIZE_X) + fromX;
+//                float y0 = ((py * (toY - fromY)) / FRAME_SIZE_Y) + fromY;
+//
+//                while (((x*x + y*y) < 4) && (iteration < MAX_ITERATION)) {
+//                    float xtemp = x*x - y*y + x0;
+//                    y = 2*x*y + y0;
+//                    x = xtemp;
+//                    iteration = iteration + 1;
+//                }
+//
+//                g.setColor(pickColor(iteration));
+//                g.drawLine(px, py, px, py);
+//
+//                loopY++;
+//                if ((loopY >= gridSize) && (loopX >= gridSize)) {
+//                    log("  x0:" + x0 + " y0:" + y0 + " iter:" + iteration);
+//                    loopY = 0;
+//                    if (gridFlag) {
+//                        g.setColor(Color.blue);
+//                        g.drawLine(px, py, px, py);
+//                    }
+//                }
+//            }
+//            if (loopX >= gridSize) {
+//                log("  -------");
+//                loopX = 0;
+//            }
+//        }
+//    }
+
     @Override
-    void drawSet(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+    int computeIteration(int px, int py) {
+        var iteration = 0;
 
-        var loopX = 0;
-        for (var px=0; px<FRAME_SIZE_X; px++) {
-            loopX ++;
-            var loopY = 0;
-            for (var py=0; py<FRAME_SIZE_Y; py++) {
-                var iteration = 0;
-                float x = 0;
-                float y = 0;
-                float x0 = ((px * (toX - fromX)) / FRAME_SIZE_X) + fromX;
-                float y0 = ((py * (toY - fromY)) / FRAME_SIZE_Y) + fromY;
+        float x = 0;
+        float y = 0;
+        float x0 = ((px * (toX - fromX)) / FRAME_SIZE_X) + fromX;
+        float y0 = ((py * (toY - fromY)) / FRAME_SIZE_Y) + fromY;
 
-                while (((x*x + y*y) < 4) && (iteration < MAX_ITERATION)) {
-                    float xtemp = x*x - y*y + x0;
-                    y = 2*x*y + y0;
-                    x = xtemp;
-                    iteration = iteration + 1;
-                }
-
-                g.setColor(pickColor(iteration));
-                g.drawLine(px, py, px, py);
-
-                loopY++;
-                if ((loopY >= gridSize) && (loopX >= gridSize)) {
-                    log("  x0:" + x0 + " y0:" + y0 + " iter:" + iteration);
-                    loopY = 0;
-                    if (gridFlag) {
-                        g.setColor(Color.blue);
-                        g.drawLine(px, py, px, py);
-                    }
-                }
-            }
-            if (loopX >= gridSize) {
-                log("  -------");
-                loopX = 0;
-            }
+        while (((x*x + y*y) < 4) && (iteration < MAX_ITERATION)) {
+            float xtemp = x*x - y*y + x0;
+            y = 2*x*y + y0;
+            x = xtemp;
+            iteration = iteration + 1;
         }
+        return iteration;
     }
 
     public Mandelbrot() throws HeadlessException {
